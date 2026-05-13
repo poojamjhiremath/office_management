2b89ccd3af3518814a38b81d04863c1efa3c48be
Pooja <poojamjhiremath@gmail.com>
2026-05-13 12:48:13 +0530

    first deploy

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,13 +1,33 @@
 from django import forms
+
 from .models import Employee, Customer
 
+
 class EmployeeForm(forms.ModelForm):
+
     class Meta:
+
         model = Employee
+
         fields = '__all__'
+
+        widgets = {
+
+            'joining_date': forms.DateInput(attrs={'type': 'date'}),
+
+        }
 
 
 class CustomerForm(forms.ModelForm):
+
     class Meta:
+
         model = Customer
+
         fields = '__all__'
+
+        widgets = {
+
+            'deadline_of_pay': forms.DateInput(attrs={'type': 'date'}),
+
+        }
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,20 +1,39 @@
 from django.db import models
 
+
 class Employee(models.Model):
+
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
+
+    joining_date = models.DateField()
+
     department = models.CharField(max_length=100)
+
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    city = models.CharField(max_length=100)
+
+    customer_name = models.CharField(max_length=100)
+
+    project_name = models.CharField(max_length=200)
+
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    paid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    balance_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+
+    deadline_of_pay = models.DateField()
+
+    def save(self, *args, **kwargs):
+
+        self.balance_amount = self.total_amount - self.paid_amount
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
+        return self.customer_name
