c4cab563aada8d11e770f87ddd933248470578e4
Pooja <poojamjhiremath@gmail.com>
2026-05-13 12:15:24 +0530

    added superuser auto create

--- build.sh
+++ build.sh
@@ -5,3 +5,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin',
+        'admin@gmail.com',
+        '123'
+    )
+END
